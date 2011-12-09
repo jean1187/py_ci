28d71e12ef0a3e4c012e86d53c10b44457da8eb4
jean <jean@jean-HP.(none)>
2011-12-09 13:49:59 -0430

    adelantos sobre la seguridad en accesos

--- application/controllers/nueva.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Nueva extends CI_Controller {
-      public function index()
-	{
-            $this->cargar->menu_system("inicio","title");
-	}
-}
--- application/controllers/nueva_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Nueva_1 extends CI_Controller {
-      public function index()
-	{
-            $this->cargar->menu_system("inicio","new_1");
-	}
-}
